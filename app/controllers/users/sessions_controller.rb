3d1987317583ffc7809f469841c2ceeea6dd4761
Elisa Burghard <codersquirrelbln@gmail.com>
2023-11-28 19:38:53 +0100

    Add devise and oauth flow

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user_info = request.env['omniauth.auth']
+    raise user_info # Your own session management should be placed here.
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :trackable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.name = auth.info.name
+      user.uid = auth.uid
+
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+end

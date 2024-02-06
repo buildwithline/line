f42ef43c19e18e08ef2245cf41b91fc5e4e753ca
Elisa Burghard <codersquirrelbln@gmail.com>
2024-02-06 10:46:11 -0700

    Appease rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_240_119_171_531) do
+ActiveRecord::Schema[7.1].define(version: 20_240_119_171_531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -21,15 +21,15 @@ ActiveRecord::Schema.define(version: 20_240_119_171_531) do
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
     t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
+    t.datetime 'reset_password_sent_at', precision: nil
+    t.datetime 'remember_created_at', precision: nil
     t.integer 'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
+    t.datetime 'current_sign_in_at', precision: nil
+    t.datetime 'last_sign_in_at', precision: nil
     t.string 'current_sign_in_ip'
     t.string 'last_sign_in_ip'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
     t.string 'provider'
     t.string 'uid'
     t.string 'nickname'

aa670baa936661673574b48c27286413c3ac39d2
Elisa Burghard <codersquirrelbln@gmail.com>
2024-03-15 21:05:14 +0100

    Update package json and credentials

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,14 @@ module Line
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+
+        resource '*',
+                 headers: :any, methods: %i[get post put patch options head delete]
+      end
+    end
   end
 end

10093428e231486941e4da99f9ea44e2588acd5f
Elisa Burghard <codersquirrelbln@gmail.com>
2024-03-20 12:57:17 +0100

    Remove commented out code

--- app/javascript/controllers/campaign_controller.js
+++ app/javascript/controllers/campaign_controller.js
@@ -11,13 +11,13 @@ export default class extends Controller {
   ]
 
   connect() {
-    this.selectedAmount = 25; // Default amount
-    this.selectedCurrency = 'USDC'; // Default currency
+    this.selectedAmount = 25;
+    this.selectedCurrency = 'USDC';
   }
 
   showCustomAmountModal() {
     this.toggleModalVisibility(true);
-    this.selectedAmount = 'Custom'; // Assuming this is how you track the selected amount
+    this.selectedAmount = 'Custom';
     this.highlightSelectedAmountButton('Custom');
   }
 
@@ -34,7 +34,6 @@ export default class extends Controller {
     const customAmount = parseFloat(this.customAmountInputTarget.value);
     if (customAmount > 0) {
       this.hideCustomAmountModal();
-      // Logic to handle valid custom amount
     } else {
       console.error("Invalid custom amount. Please enter a number greater than 0.");
     }
@@ -55,10 +54,6 @@ export default class extends Controller {
   highlightSelectedAmountButton(selectedAmount) {
     this.amountButtonTargets.forEach(button => {
       const isSelected = button.dataset.campaignValue === selectedAmount || (selectedAmount === 'Custom' && button.dataset.campaignValue === 'Custom');
-      console.log('what is selected?')
-      console.log('selectedAmount === "Custom"', selectedAmount === "Custom")
-      console.log("button.dataset.campaignValue === 'Custom'", button.dataset.campaignValue === 'Custom')
-      console.log('isselected', isSelected)
       button.classList.toggle("bg-blue-500", isSelected);
       button.classList.toggle("text-white", isSelected);
       button.classList.toggle("bg-white", !isSelected);
@@ -89,64 +84,4 @@ export default class extends Controller {
     alert(`Thank you for your support of ${this.selectedAmount} ${this.selectedCurrency}!`);
     // Extend with actual logic for supporting the campaign
   }
-  // updateCustomAmountVisibility() {
-  //   if (this.selectedAmount === 'Custom') {
-  //     this.showCustomAmountModal();
-  //   } else {
-  //     this.hideCustomAmountModal();
-
-  //   }
-  // }
-
-  // confirmCustomAmount() {
-  //   // Get the custom amount from the input field
-  //   const customAmount = parseFloat(this.customAmountInputTarget.value);
-  //   if (customAmount > 0) {
-  //     this.hideCustomAmountModal();
-  //     // Update the UI with the custom amount
-  //     // this.updateContributionDisplay(customAmount);
-  //   } else {
-  //     // Show an error message or handle invalid input
-  //     console.log("Invalid custom amount. Please enter a number greater than 0.");
-  //   }
-  // }
-
-  // cancelCustomAmount() {
-  //   this.hideCustomAmountModal();
-  //   this.customAmountModalTarget.classList.add('hidden');
-  // }
-
-  // selectAmount(event) {
-  //   const selectedAmount = event.currentTarget.dataset.campaignValue;
-  //   this.selectedAmount = selectedAmount;
-  //   this.amountButtonTargets.forEach((button) => {
-  //       if (button.dataset.campaignValue === selectedAmount) {
-  //           button.classList.add("bg-blue-500", "text-white");
-  //           button.classList.remove("bg-white", "text-black");
-  //       } else {
-  //           button.classList.remove("bg-blue-500", "text-white");
-  //           button.classList.add("bg-white", "text-black");
-  //       }
-  //   });
-  //   this.updateCustomAmountVisibility(); 
-  // }
-
-  // selectCurrency(event) {
-  //   const selectedCurrency = event.currentTarget.dataset.campaignValue;
-  //   this.currencyTargets.forEach((button) => {
-  //       if (button.dataset.campaignValue === selectedCurrency) {
-  //           button.classList.add("bg-blue-500", "text-white");
-  //           button.classList.remove("bg-white", "text-black");
-  //       } else {
-  //           button.classList.remove("bg-blue-500", "text-white");
-  //           button.classList.add("bg-white", "text-black");
-  //       }
-  //   });
-  // }
-
-  // supportCampaign() {
-  //   // This method simulates a support action
-  //   alert(`Thank you for your support of ${this.selectedAmount} ${this.selectedCurrency}!`);
-  //   // Here you can add actual logic to handle the campaign support action, such as making an API call
-  // }
 }

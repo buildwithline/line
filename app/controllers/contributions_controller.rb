ccd6281244f4d60949abd7280995072ce3fd9bcd
Elisa Burghard <codersquirrelbln@gmail.com>
2024-04-14 20:18:19 +0200

    update contributions controller

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -3,13 +3,28 @@
 class ContributionsController < ApplicationController
   before_action :authenticate_user!
 
-  def new; end
+  def new
+    pp 'new action'
+    @campaign = Campaign.find(params[:campaign_id])
+    pp '++++++++++++++++++++'
+    pp @campaign
+    pp '++++++++++++++++++++'
+    @contribution = Contribution.new
+    @accepted_currencies = @campaign.accepted_currencies.split(',')
+  end
 
-  def create; end
+  def create
+    pp 'create contrib'
+  end
 
   private
 
+  def set_user
+    pp 'set user in contrib'
+    @user = current_user
+  end
+
   def contributions_params
-    params.require(:campaign).permit(:contribution_cadence, :accepted_currency, :repo_identifier, :receiving_wallet_id)
+    params.require(:contribution).permit(:contribution_cadence, :accepted_currencies, :repo_identifier, :receiving_wallet_id)
   end
 end

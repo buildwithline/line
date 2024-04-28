0a830ce554de8681e100470ad961140961dbd3c2
Elisa <codersquirrelbln@gmail.com>
2024-04-28 21:50:23 +0200

    Add application file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,22 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+// import { createApp } from 'node_modules/vue/dist/vue.esm-bundler.js';
+// import CampaignButton from './components/CampaignButton.vue';
 
-import { createApp } from 'vue';
-import CampaignButton from './components/CampaignButton.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM content loaded');
-    const app = createApp(CampaignButton);
-    console.log('Vue app created:', app);
-    app.mount('#vue-app');
-});
+createApp(App).mount('#vue-app')
+// document.addEventListener('DOMContentLoaded', () => {
+//     const app = createApp({
+//         components: {
+//             'campaign-button': CampaignButton
+//         },
+//         template: `<div>
+//                        <campaign-button repo-name="YourRepoName"></campaign-button>
+//                    </div>`
+//     });
+
+//     console.log('Vue app created', app);
+//     app.mount('#vue-app');
+// });

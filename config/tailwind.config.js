d60a59afe374d552815ca23b0f541967a16bc1a6
Elisa <codersquirrelbln@gmail.com>
2024-06-27 12:56:14 -0600

    Add stylesheets to tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*',
+    './app/assets/stylesheets/**/*.css',
   ],
   theme: {
     extend: {},
